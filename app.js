43c160e2d88bb9312767347e4758583b5488a389
Eric Tai <ericloong90@gmail.com>
2025-07-31 09:00:08 +0000

    Adds exclamation mark to message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: "Hello Eric Tai" });
+  res.send({ message: "Hello Eric Tai!" });
 });
 
 module.exports = app;
